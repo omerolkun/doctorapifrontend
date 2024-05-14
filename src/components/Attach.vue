828945d811d5ab7fd6af7398932c712364953e75
omerolkun <omerolkun2004@gmail.com>
2024-05-14 16:41:54 +0300

    create list

--- src/components/Attach.vue
+++ src/components/Attach.vue
@@ -4,18 +4,38 @@
         <form @submit="postData" method="post">
             <div>
                 <label for="doctor">Doktor TCKN: </label>
-                <input type="text" id="doctor" name="doctor" v-model="posts.doctor">
+                <input type="text" id="doctor" name="doctor" v-model="posts.doctorid">
             </div>
             <div>
                 <label for="hospital">hastanesi</label>
-                <select id="hospital" name="hospital" v-model="posts.hospital">
-                    <option value="item.id" v-for="item in apiData" :key="item.id">{{ item.name}} </option>
+                <select id="hospital" name="hospital" v-model="posts.hospitalid">
+                    <option v-for="item in apiData" v-bind:value="item.id">
+                        {{ item.name }}
+                    </option>
                 </select>
             </div>
             <div>
                 <button type="submit">Save</button>
             </div>
 
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>sira</th>
+                            <th>doctorId</th>
+                            <th>hospitalId</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(item,index) in junctionTableData" :key="item.id">
+                            <td>{{ index+ 1 }}</td>
+                            <td>{{ item.doctorId }}</td>
+                            <td>{{ item.hospitalId }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </form>
 
     </div>
@@ -30,20 +50,30 @@ export default {
     data() {
         return {
             posts: {
-                doctor: null,
-                hospital: null
+                doctorid: null,
+                hospitalid: null
             },
-            apiData: []
+            apiData: [],
+            junctionTableData: []
         }
     },
     mounted() {
         this.getHospitalData();
+        this.getJunctionTableData();
     },
     methods: {
         getHospitalData() {
             axios.get('http://localhost:5179/api/hospital')
                 .then((response) => {
                     this.apiData = response.data;
+                })
+                .catch((error) => { console.error('Error: ', error); });
+        },
+        getJunctionTableData() {
+            axios.get('http://localhost:5179/api/doctorhospital')
+                .then((response) => {
+                    this.junctionTableData= response.data;
+               //     console.log(junctionTableData);
                 })
                 .catch((error) => { console.error('Error: ', error); });
         },
